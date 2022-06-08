6008228eeab8ff2a44bc6906efd9b538264d5ec7
Vitaliy4D <dalvitaliy@gmail.com>
2022-06-08 13:35:33 +0300

    added my practice of linear regression

---
+++ LinearRegression.py
@@ -0,0 +1,162 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
+import seaborn as sns
+
+# load and inspect data
+dataset = pd.read_csv('C:/ML/Housing Prices Competition/train.csv')
+
+dataset.head()
+
+
+# plot features correlation in order to choose most correlated feature for analysis
+# SalePrice is our dependable variable
+
+dataset.corr()[['SalePrice']].sort_values(by='SalePrice', ascending=False)
+d_corr = dataset.corr()[['SalePrice']].sort_values(by='SalePrice', ascending=False)
+
+plt.figure(figsize=(10, 8))
+heatmap = sns.heatmap(dataset.corr()[['SalePrice']].sort_values(by='SalePrice', ascending=False), vmin=-1, vmax=1, annot=True, cmap='BrBG')
+
+heatmap.set_title('Features Correlating with SalePrice', fontdict={'fontsize':18}, pad=16)
+
+
+# inspect if our choosen feature has no NA
+
+dataset['OverallQual'].isna().sum()
+
+
+# vizualise our x and y
+
+x = dataset['OverallQual']
+y = dataset['SalePrice']
+plt.scatter(x, y)
+plt.xlabel('OverallQual')
+plt.ylabel('SalePrice')
+plt.title('OverallQual vs SalePrice')
+plt.show() 
+
+
+# Scipy: Calculate a linear least-squares regression for two sets of measurements
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+res = stats.linregress(x, y)
+print(f"R-squared: {res.rvalue**2*100:.4f}")
+
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
+plt.plot(x, y, 'o', label='original data')
+plt.plot(x, mymodel, 'r', label='fitted line')
+plt.legend()
+plt.show() 
+
+
+# Calculate LinearRegression with sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+# loading data and splitting to train and test
+
+dataset = pd.read_csv('C:/ML/Housing Prices Competition/train.csv')
+
+x = dataset['OverallQual'].array.reshape(-1, 1) 
+y = dataset['SalePrice'].array.reshape(-1, 1) 
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=.7, random_state=1)
+
+regr = LinearRegression()
+
+
+# Fitting Simple Linear Regression to the Training set
+regr.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regr.predict(X_test)
+
+
+# ploting the training dataset in scattered graph
+plt.scatter(X_train, y_train, color='blue')
+
+# ploting the testing dataset in line line
+plt.plot(X_train, regr.predict(X_train), color='red')
+plt.title('Prices vs OverallQual')
+
+# labeling the input and outputs
+plt.xlabel('OverallQual')
+plt.ylabel('SalePrice')
+
+# showing the graph
+plt.show()
+
+
+# Visualysing the Test set results
+viz_test = plt
+
+# red dot colors for actual values
+viz_test.scatter(X_test, y_test, color='blue')
+
+# Blue line for the predicted values
+viz_test.plot(X_test, regr.predict(X_test), color='red')
+
+# defining the title
+viz_test.title('OverallQual vs SalePrice')
+
+# x lable
+viz_test.xlabel('OverallQual')
+
+# y label
+viz_test.ylabel('SalePrice')
+
+# showing the graph
+viz_test.show()
+
+
+# Importing metrics from sklearn module to evaluate our model
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error,mean_squared_error,r2_score 
+from math import sqrt
+
+print('R squared train: {:.2f}'.format(regr.score(X_train, y_train)*100))
+print('R squared test: {:.2f}'.format(regr.score(X_test, y_test)*100))
+
+# printing the mean absolute error
+print(f'mean_absolute_error train: {mean_absolute_error(X_train, regr.predict(X_train)):.2f}') # actual_values, predicted_values
+
+# printing the mean absolute error
+print(f'mean_absolute_error test: {mean_absolute_error(y_test, y_pred):.2f}')
+
+# printing the mean squared error
+print(f'mean_squared_error train: {mean_squared_error(X_train, regr.predict(X_train)):.2f}') # actual_values, predicted_values
+
+# printing the mean squared error
+print(f'mean_squared_error test: {mean_squared_error(y_test, y_pred):.2f}')
+
+# printing the root mean absolute error
+print(f'Root Mean Square Error train: {sqrt(mean_squared_error(X_train, regr.predict(X_train))):.2f}') # actual_values, predicted_values
+
+# printing the mean squared error
+print(f'Root Mean Square Error test: {sqrt(mean_squared_error(y_test, y_pred)):.2f}')
+
+
+# applying r square error
+# The R-Squared Error method is also known as 
+# the coefficient of determination. 
+# This metric indicates how well a model fits a given dataset. 
+# Or in simple words, it indicates how close the regression line 
+# is to the actual data values.
+R_square = r2_score(X_train, regr.predict(X_train)) # actual_values, predicted_values
+print(R_square)
+
+
+# applying r square error, % how close our model predict 
+R_square_train = r2_score(y_train, regr.predict(X_train))*100
+R_square = r2_score(y_test, y_pred)*100
+
+print(R_square_train)
+print(R_square)
---
+++ MultipleLinearRegression.py
@@ -0,0 +1,199 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# load dataset
+dataset = pd.read_csv("C:/ML/Housing Prices Competition/train.csv")
+
+# let do some exploration of dataset
+dataset.head()
+
+dataset.describe()
+
+#### let see and draw some barplot how many features with NA
+col_na=dataset.columns[dataset.isna().any()].tolist()
+
+col_na_sum=[]
+for i in col_na:
+    col_na_sum.append(dataset[i].isna().sum())
+    
+df_col_na = ({
+    'colnames':col_na,
+    'na_quantity':col_na_sum
+})
+
+print(df_col_na)
+
+import matplotlib.pyplot as plt
+
+x_na=col_na
+y_na=col_na_sum
+
+fig, ax = plt.subplots(figsize=(8, 5))
+bars = ax.barh(x_na, sorted(y_na), color='green')
+ax.bar_label(bars)
+plt.title('Columns with NA')
+plt.xlabel('quantity of NA')
+plt.ylabel('Columns names')
+plt.show
+####
+
+
+# have a look at dependent variable
+sns.histplot(dataset['SalePrice'])
+
+# let me see what is the features correlation
+dataset.corr()[['SalePrice']].sort_values(by='SalePrice', ascending=False)
+d_corr = dataset.corr()[['SalePrice']].sort_values(by='SalePrice', ascending=False)
+
+plt.figure(figsize=(10, 8))
+heatmap = sns.heatmap(dataset.corr()[['SalePrice']].sort_values(by='SalePrice', ascending=False), vmin=-1, vmax=1, annot=True, cmap='BrBG')
+
+heatmap.set_title('Features Correlating with SalePrice', fontdict={'fontsize':18}, pad=16)
+
+
+
+# let us choose columns where correlation greater that 0,05 to make a regression
+col_corr = d_corr.loc[d_corr['SalePrice'] >= 0.05]
+col_corr_names= col_corr.index[1:].tolist()
+    
+print(col_corr_names)
+
+# inspect the data of choosen correlated columns
+pd.set_option('display.max_columns', None)
+dataset[col_corr_names].head()
+
+# let's check what type of data in the columns? 
+dataset[col_corr_names].dtypes
+
+# Linear regression doesn't work on date data. Therefore we need to convert it 
+# into numerical value.The following code will convert the date into numerical value:
+
+# import datetime as dt
+# data_df['Date'] = pd.to_datetime(data_df['Date'])
+# data_df['Date']=data_df['Date'].map(dt.datetime.toordinal)
+
+# df.date = df.date.map(datetime.toordinal)
+
+# convert back to datetime
+# pd.to_datetime('20220101', format='%Y%m%d', errors='coerce')
+
+# checking if choosen columns have NA,  include='all', exclude=[np.number], include = [np.number]
+dataset[col_corr_names].describe(include=[None])
+
+# alternative check if correlated columns have NA
+# dataset[col_corr_names].columns[dataset[col_corr_names].isna().any()].tolist()
+
+# few columns have NA, let's count na in columns
+[dataset[i].isna().sum() for i in ['GarageYrBlt','MasVnrArea','LotFrontage']]
+
+# now let see how many % of total of na in the column
+[(dataset[i].isna().sum()/dataset[i].count())*100 for i in ['GarageYrBlt','MasVnrArea','LotFrontage']]
+
+# define what is correlation of these columns, 
+# is it important to include it into our mult.regression
+d_corr.loc[['GarageYrBlt', 'MasVnrArea', 'LotFrontage']]
+
+# for feature GarageYrBlt - would use mode(The most common value) to impute NA, 
+# hist is not equally distrib and amount of NA is not so big
+sns.histplot(dataset['GarageYrBlt']) 
+
+# for MasVnrArea - would also use mode(The most common value) to impute NA, hist is not equally distrib
+sns.histplot(dataset['MasVnrArea']) 
+
+# column LotFrontage - I should drop it, because 21% of NA is too many and it can influence the final result
+# but it seems hist looks like normal so I will use mean for experimental purposes
+sns.histplot(dataset['LotFrontage']) 
+
+# let's fill na in three columns
+dataset['GarageYrBlt']=dataset['GarageYrBlt'].fillna(dataset['GarageYrBlt'].mode().iloc[0])
+dataset['MasVnrArea']=dataset['MasVnrArea'].fillna(dataset['MasVnrArea'].mode().iloc[0])
+dataset['LotFrontage']=dataset['LotFrontage'].fillna(dataset['LotFrontage'].mean())
+
+# count na in columns one more time to be sure fill na is done
+[dataset[i].isna().sum() for i in ['GarageYrBlt','MasVnrArea','LotFrontage']]
+
+# as alternative to imputation, we can drop columns with NA from our dataset, 
+# if it's correlation small (less than 0.05)
+
+# list_corr_na = dataset[col_corr_names].columns[dataset[col_corr_names].isna().any()].tolist()
+# fin_corr_list = [x for x in col_corr_names if not x in list_corr_na or list_corr_na.remove(x)]
+# print(fin_corr_list)
+
+# set variables for our regression
+X_t = dataset[col_corr_names]
+y_t=dataset['SalePrice'].values
+
+# let's make test/train split
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(X_t, y_t, test_size = 0.3, random_state = 100)
+
+#Fitting the Multiple Linear Regression model
+from sklearn.linear_model import LinearRegression
+
+mlr = LinearRegression()  
+mlr.fit(x_train, y_train)
+
+# let check Intercept and Coefficients for our selected features
+from operator import itemgetter
+
+print("Intercept: ", mlr.intercept_)
+print("Coefficients:")
+list(sorted(zip(fin_corr_list, mlr.coef_),key=itemgetter(1),reverse=True))
+
+#Prediction of test set
+y_pred_mlr= mlr.predict(x_test)
+
+#Predicted values
+print(f"Prediction for test set: {y_pred_mlr}")
+
+#Actual value and the predicted value, lists are flattened from list of lists
+# mlr_diff = pd.DataFrame({'Actual value': [x for xs in y_test.values for x in xs], 'Predicted value': [x for xs in y_pred_mlr for x in xs]})
+# mlr_diff.head()
+mlr_diff = pd.DataFrame({'Actual value': y_test, 'Predicted value': y_pred_mlr})
+print(mlr_diff)
+
+#Model Evaluation
+from sklearn import metrics
+meanAbErr = metrics.mean_absolute_error(y_test, y_pred_mlr)
+meanSqErr = metrics.mean_squared_error(y_test, y_pred_mlr)
+rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_test, y_pred_mlr))
+print('R squared trai: {:.2f}'.format(mlr.score(X_t, y_t)*100))
+print('R squared: {:.2f}'.format(mlr.score(x_test, y_test)*100))
+print('Mean Absolute Error:', meanAbErr)
+print('Mean Square Error:', meanSqErr)
+print('Root Mean Square Error:', rootMeanSqErr)
+
+# R Squared: R Square is the coefficient of determination. 
+# It tells us how many points fall on the regression line. 
+# The value of R Square is 79.72 for train data amd 80.67 for our test data
+# which indicates that 79.72% of the data fit the regression model.
+
+# Mean Absolute Error: Mean Absolute Error is the absolute difference 
+# between the actual or true values and the predicted values. 
+# The lower the value, the better is the model’s performance. 
+# A mean absolute error of 0 means that your model is 
+# a perfect predictor of the outputs. 
+# The mean absolute error obtained for this particular model is 22960, 
+# which is not so good as it is not really close to 0.
+
+# Mean Square Error: Mean Square Error 
+# is calculated by taking the average of the square of the difference 
+# between the original and predicted values of the data. 
+# The lower the value, the better is the model’s performance. 
+# The mean square error obtained for this particular model is 1244157729, 
+# which is not really good
+
+# Root Mean Square Error: Root Mean Square Error 
+# is the standard deviation of the errors which occur 
+# when a prediction is made on a dataset. 
+# This is the same as Mean Squared Error, 
+# but the root of the value is considered 
+# while determining the accuracy of the model. 
+# The lower the value, the better is the model’s performance. 
+# The root mean square error obtained for this particular model 
+# is 35272, which is not so good.
+
+# for this particular set it is better to try some other ML models
